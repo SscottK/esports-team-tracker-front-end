ac77b87e1816c297da28c31c683404d700608652
ScottKinnear <gr8whtninjassk@gmail.com>
2024-10-10 22:40:49 -0700

    added react-bootstrap for a table, future styling

--- src/components/Team/TeamGrid.jsx
+++ src/components/Team/TeamGrid.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
+import Table from 'react-bootstrap/Table';
 
 const TeamGrid = ({ user, team, gameId }) => {
     const [member, setMember] = useState(null)
     const [game, setGame] = useState(null)
     
     console.log(member)
-    useEffect(() => {
-        const prepareMember = () => {
-            setMember(team.members[1]) 
-        }
-       if (user) prepareMember()
-        // console.log(member.trackedTimes)
-    }, [user])
+    // useEffect(() => {
+    //     const prepareMember = () => {
+    //         setMember(team.members[1]) 
+    //     }
+    //    if (user) prepareMember()
+    //     // console.log(member.trackedTimes)
+    // }, [user])
     // if (game) {
     //     const foundMember = team.members.find((member) => {
     //         return game._id === event.target.value
@@ -22,17 +23,50 @@ const TeamGrid = ({ user, team, gameId }) => {
     return (
         <>
             <h1>This is the team grid</h1>
-            <div className="container">
-                {team ? team.members.map((member) => {                    
-                    return <div key={member._id} className="name">{member.username}<div>{team ? member.trackedTimes.map((time) => {
+            {/* {team ? member.trackedTimes.map((time) => {
                         return <div key={time._id}className="time">{time}</div>
-                    }) : ''}</div></div>
-                    
-                }): ''
-                }
-                
+                    }) : ''} */}
+            <Table striped bordered hover responsive>
+      <thead>
+        <tr >
+            <th>Track Name</th>
+        {team ? team.members.map((member) => {                    
+                    return <th key={member._id} className="nameTable">{member.username}</th>
+                    }) : ''
+                    }
+          </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>1</td>
+          <tc>
+          
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          </tc>
+        
 
-            </div>
+        </tr>
+        <tr>
+        
+          <td>2</td>
+          <td></td>
+          <tc>
+          <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>          
+          </tr></tc>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+      
+    </Table>
         </>
         
         
